60cbb37ef7b541d031107863574e6182c65370d8
JordanHempstead <tariqhempst@gmail.com>
2018-03-06 13:43:28 -0700

    Add files via upload

---
+++ python_dailyexercise-01.py
@@ -0,0 +1,3 @@
+#Jordan Hempstead - Python Daily Exercise 01 2/12/2018
+#1.
+print ("Bing" * 2 + "Bong" * 2)
---
+++ python_dailyexercise-02.py
@@ -0,0 +1,8 @@
+#Jordan Hempstead - Python Daily Exercise 02 2/12/2018
+print ("What is your name?: ")
+name= input()
+print("What is your age?: ")
+age= input()
+print ((name) + " you  will be 100 in")
+print (100 - int(age))
+
