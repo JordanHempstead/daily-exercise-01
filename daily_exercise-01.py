d580df9d99659911a04403fdf1cf5f3a38ed120d
JordanHempstead <tariqhempst@gmail.com>
2018-03-06 13:36:39 -0700

    Delete daily_exercise-01.py

--- daily_exercise-01.py
+++
@@ -1,9 +0,0 @@
-#Jordan Hempstead - Python Daily Exercise 01 2/12/2018
-#1.
-print ("Bing" * 2 + "Bong" * 2)
-#2.
-print ("What is your name?: ")
-name= input()
-print("What is your age?: ")
-age= input()
-print ((name) + " you  will be 100 in" + (100 - age))
